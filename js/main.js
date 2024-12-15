b4594f85197168d0e3e63180e48f444857cc669b
hunterxray <kunk89296@gmail.com>
2024-12-16 00:03:11 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('FOR SAFIRA').split('')
+    const titles = ('I LOVE YOI').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
